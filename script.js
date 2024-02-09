4ebfce7512a84f7c5a7e59ac1c5cfceddf4a1f11
ChiTart <chiaratartaglia.ct@gmail.com>
2024-02-09 17:36:08 +0100

    Enter Key abled, fixed checkbox, added comments
    - Added comments inside the code
    - Fixed the toggle between checked and unchecked tasks
    - Added the option to add tasks by clicking on the Enter key

--- script.js
+++ script.js
@@ -5,21 +5,47 @@ function AddTask(){
     if(inputBox.value === "") {
         alert("You must write something!");
     } else {
+        // Create a new <li> element
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+
+        // Create an <i> element that is a blank checkbox
+        let checkboxIcon = document.createElement('i');
+        checkboxIcon.className = 'ri-checkbox-blank-circle-line';
+
+        // Create the X needed to delete a task within a <span> element
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+
+        // Append <i>, task text and <span> to the <li> element
+        li.appendChild(checkboxIcon)
+        li.innerHTML += inputBox.value;
         li.appendChild(span);
+
+        // Append the <li> element to the list
+        listContainer.appendChild(li);
     }
+
+    // Clear the input box and save data
     inputBox.value = "";
     saveData();
 }
 
+// Call the AddTask function when writing in the input box and pressing the Enter key
+inputBox.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        AddTask();
+    }
+});
+
+// Check list items when clicked + remove them when the user click the X
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
-        saveData()
+
+        let checkboxIcon = e.target.querySelector('i');
+        toggleCheckboxClass(checkboxIcon);
+
+        saveData();
     } else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData()
@@ -28,6 +54,12 @@ listContainer.addEventListener("click", function(e){
 
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// Change between checked and unchecked checkbox
+function toggleCheckboxClass(checkboxIcon) {
+    checkboxIcon.classList.toggle("ri-checkbox-blank-circle-line");
+    checkboxIcon.classList.toggle("ri-checkbox-circle-line");
 }
 
 function showTask() {
